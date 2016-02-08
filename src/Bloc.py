d63d43ab3e3382124069605152f7d3f35010235c
IhFisk <ihfisk42@gmail.com>
2016-02-08 13:38:11 +0100

    Create Bloc.py

---
+++ src/Bloc.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Bloc(Enum):
+    Eau = 0
+    Sable = 1
+    Herbe = 2
+    Plancher = 3
+    Birque = 4
+    AutorouteA76DirectionDijon = 5
+    PetitPortillonMignon = 6
