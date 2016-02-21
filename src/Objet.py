63ae253d34c118d02cfe17647b1d82c116c23b66
Sami Grolleau <ceytix@mailoo.org>
2016-02-21 15:58:27 +0100

    c'est dans 10 ans, je m'en irai

---
+++ src/Objet.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Objet(Enum):
+    Rien = 0
+    Banc = 1
+    CheckPoint = 2
+    Lit = 3
+    ChaiseEcole = 4
+    TableEcole = 5
+    Four = 6
+    EntreeFour = 7
+    Chaise = 8
+    Table = 9
+    PlaceProf = 10
+    Tableau = 11
+    LitMedecin = 12
+    ChaiseMedecin = 13
+    LitPsychiatre = 14
+    ChaisePsychiatre = 15
+    BancPeche = 16
