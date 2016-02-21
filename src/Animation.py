ab2133ae16de11a5291d234c223c99a8d546797f
Sami Grolleau <ceytix@mailoo.org>
2016-02-21 15:20:03 +0100

    Update Animation.py

--- src/Animation.py
+++ src/Animation.py
@@ -1,5 +1,16 @@
+import sfml as sf
+
 class Animation:
     
     def __init__(self, tableau, temps):
         self.tableau = tableau
         self.temps = temps
+
+
+    def play(self, fenetre):
+        fenetre.clear(sf.Color.White)
+        
+        for i in range(len(tableau)):
+            sf.draw(tableau[i])
+            sf.display()
+            sf.sleep(temps)
