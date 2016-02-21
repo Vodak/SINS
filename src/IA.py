ea0c01fe113b68ea992365d4741b2426135a390c
Sami Grolleau <ceytix@mailoo.org>
2016-02-21 16:30:48 +0100

    Update IA.py

--- src/IA.py
+++ src/IA.py
@@ -1,4 +1,17 @@
 class IA:
     
     def __init__(self):
-        self.age = 1
+        self._vie = 100
+        self._fatigue = 0
+        self._faim = 0
+        self._bonheur = 100
+        self._maladie = 0
+        
+        self._charisme = 0
+        self._intelligence = 0
+        self._force = 0
+        self._soumission =0
+        self._endurance = 0
+        
+        self._age = 0
+        self._sexe = "rien"
