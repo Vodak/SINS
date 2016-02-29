1f499af6c7d21bad6e7355d6b19e693e5215f822
Sami Grolleau <ceytix@mailoo.org>
2016-02-29 13:53:47 +0100

    Update Animation.py

--- src/Animation.py
+++ src/Animation.py
@@ -1,14 +1,23 @@
+"""
+Classe animation permettant de créer et d'utiliser des animations à partir d'images et de temps entre elles
+"""
+
 import sfml as sf
 
 class Animation:
+    
+    # Constructeur
     
     def __init__(self, tableau, temps):
         self.tableau = tableau
         self.temps = temps
 
-
-    def play(self, fenetre):
+    # Classe utiliser lançant l'animation
+    
+    def utiliser(self, fenetre):
         fenetre.clear(sf.Color.White)
+        
+        # Chaque image est affichée un certain temps
         
         for i in range(len(tableau)):
             sf.draw(tableau[i])
