1b2aaac8155df909c596276b54a39ccaaaba9c92
IhFisk <ihfisk42@gmail.com>
2016-03-06 22:41:48 +0100

    This is a temporary file

---
+++ src/testCollision.py
@@ -0,0 +1,16 @@
+from map import *
+
+generate()
+
+x = 0
+y = 0
+maison = map[x][y]
+
+for i in range 96:
+	for j in range 72 :
+		
+		x = i
+		y = j
+		
+		for k in range 6 :
+			for k in range 9 :
