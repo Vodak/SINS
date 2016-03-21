0b862246d6b0b81b969d8422b35b0263f2d29200
IhFisk <ihfisk42@gmail.com>
2016-03-21 21:36:56 +0100

    Create testAmenagement.py

---
+++ src/testAmenagement.py
@@ -0,0 +1,17 @@
+"""
+Fichier provisoire
+"""
+
+from Map import *
+
+carte = Map()
+carte.generate()
+
+(x, y) = (5, 5)
+
+pointA = (x, y)
+pointB = (x + 9, y + 6)
+
+route = False
+positionPorte = (x, y)
+placable = True
