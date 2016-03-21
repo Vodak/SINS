23bb443dda87ef568a3070edc9f290a3d021faf7
Sami Grolleau <ceytix@mailoo.org>
2016-03-21 14:24:46 +0100

    Update testCollision.py

--- src/testCollision.py
+++ src/testCollision.py
@@ -15,26 +15,26 @@ route = False
 positionPorte = (x, y)
 placable = True
 
-for i in range(x, x + 9):
-	if map[i][y - 2] == Bloc.Route:
+for i in range(x + 1, x + 8):
+	if map[i][y - 1] == Bloc.Route:
 		if not route:
-			positionPorte = (i, y - 2)
+			positionPorte = (i, y - 1)
 		route = True
 		
-	elif map[i][y + 8] == Bloc.Route:
+	elif map[i][y + 7] == Bloc.Route:
 		if not route:
-			positionPorte = (i, y + 8)
+			positionPorte = (i, y + 7)
 		route = True
 		
-for j in range(y, y + 6):
-	if map[x -2][j] == Bloc.Route:
+for j in range(y + 1, y + 5):
+	if map[x - 1][j] == Bloc.Route:
 		if not route:
-			positionPorte = (x - 2, j)
+			positionPorte = (x - 1, j)
 		route = True
 	
-	if map[x + 11][j] == Bloc.Route:
+	if map[x + 10][j] == Bloc.Route:
 		if not route:
-			positionPorte = (x + 11, j)
+			positionPorte = (x + 10, j)
 		route = True
 	
 for i in range(x , x + 9):
