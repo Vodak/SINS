1a706e99ae44b8b2e89b393d3d613bb60d964d09
Sami Grolleau <ceytix@mailoo.org>
2016-04-11 22:41:54 +0200

    Create Action.py

---
+++ src/Action.py
@@ -0,0 +1,29 @@
+"""
+Classe Action qui est une énumeration
+"""
+
+from enum import Enum
+
+class Action(Enum):
+    Sasseoir = 0
+    Marcher = 1
+    Dormir = 2
+    Cuire = 3
+    Manger = 4
+    Enseigner = 5
+    EtreSoigne = 6
+    Soigner = 7
+    EtreSoignePsy = 8
+    SoignerPsy = 9
+    Pecher = 10
+    
+    Frapper = 11
+    SeFaireFrapper = 12
+    Parler = 13
+    Repondre = 14
+    Fuir = 15
+    Embrasser = 16
+    SeReproduire = 17
+    Facher = 18
+    Apprendre = 19
+    Enseigner = 20
